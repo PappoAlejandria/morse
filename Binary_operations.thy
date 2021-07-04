f9774cbea2a98ad8e3c99426fbf7233cdb0012bb
PappoAlejandria <60967393+PappoAlejandria@users.noreply.github.com>
2021-07-04 18:28:41 +0200

    Add files via upload

--- Binary_operations.thy
+++ Binary_operations.thy
@@ -30,46 +30,55 @@ lemma snd_es_simplice:
 definition set_ast :: "('a set) \<Rightarrow> ('b set) \<Rightarrow> (('a*'b) set)"
   where "set_ast A B \<equiv> {c. \<exists>a\<in>A. \<exists>b\<in>B. c = (a,b)}"
 
-definition set_fst :: "('a::class_mod_type*'b::class_mod_type) set \<Rightarrow> 'a set"
+definition set_fst :: "('a*'b) set \<Rightarrow> 'a set"
   where "set_fst AB = {a. \<exists>ab\<in>AB. a = fst ab}"
 
-value "set_fst {a, b}"
-
-lemma set_fst_simp [simp]: "set_fst (x \<times> y) = x"
+lemma set_fst_simp [simp]:
+  assumes "y \<noteq> {}"
+  shows "set_fst (x \<times> y) = x"
 proof
   show "set_fst (x \<times> y) \<subseteq> x"
     by (smt (verit) SigmaE mem_Collect_eq prod.sel(1) set_fst_def subsetI)
   show "x \<subseteq> set_fst (x \<times> y)"
-  proof
-    fix a::"'a"
-    assume "a \<in> x"
-    then have "\<exists>b\<in>y. (a,b)\<in>(x\<times>y)"
-      by auto
-    thus "a \<in> set_fst (x \<times> y)"
-      by auto
-  qed
+    using assms set_fst_def by fastforce
 qed
 
 definition set_snd :: "('a*'b) set \<Rightarrow> 'b set"
   where "set_snd AB = {b. \<exists>ab\<in>AB. b = snd(ab)}"
 
+lemma set_snd_simp [simp]:
+  assumes "x \<noteq> {}"
+  shows "set_snd (x \<times> y) = y"
+proof
+  show "set_snd (x \<times> y) \<subseteq> y"
+    by (smt (verit) SigmaE mem_Collect_eq prod.sel(2) set_snd_def subsetI)
+  show "y \<subseteq> set_snd (x \<times> y)"
+    using assms set_snd_def by fastforce
+qed
+
 definition bool_fun_times :: "(bool^('n::class_mod_type) \<Rightarrow> bool) \<Rightarrow> (bool^('m::class_mod_type) \<Rightarrow> bool) \<Rightarrow> (bool^('n*'m) \<Rightarrow> bool)"
   where "(bool_fun_times f g) x \<equiv>
      f (bool_vec_from_simplice (set_fst (ceros_of_boolean_input x)))
-   \<and> g (bool_vec_from_simplice (set_snd (ceros_of_boolean_input x)))"
-
-(* La siguiente condición causa que el teorema pertinente sea incorrecto:
- \<and> (\<nexists>c. \<exists>d\<in>(ceros_of_boolean_input x). c\<in>(ceros_of_boolean_input x) \<and> (fst c < fst d) \<and> (snd c > snd d))*)
+   \<and> g (bool_vec_from_simplice (set_snd (ceros_of_boolean_input x)))
+ \<and> (\<nexists>c. \<exists>d\<in>(ceros_of_boolean_input x). c\<in>(ceros_of_boolean_input x) \<and> (fst c < fst d) \<and> (snd c > snd d))"
 
 definition
   simplicial_complex_induced_by_monotone_boolean_function_times 
-    :: "(bool^('n::class_mod_type*'m::class_mod_type) \<Rightarrow> bool) => (('n set)*('m set)) set"
+    :: "(bool^('n::class_mod_type*'m::class_mod_type) \<Rightarrow> bool) => ('n*'m) set set"
   where "simplicial_complex_induced_by_monotone_boolean_function_times f = 
-        {z. \<exists>x y. f (bool_vec_from_simplice (x \<times> y)) \<and> (x, y) = z}"
+        {z. \<exists>x y. f (bool_vec_from_simplice ((ceros_of_boolean_input x) \<times> (ceros_of_boolean_input y))) \<and> (ceros_of_boolean_input x \<times> ceros_of_boolean_input y) = z}"
+
+value "bool_vec_from_simplice ({a,b})"
 
 lemma simp_ceros [simp]: "ceros_of_boolean_input (bool_vec_from_simplice (x \<times> y)) = x \<times> y"
   unfolding bool_vec_from_simplice_def
   unfolding ceros_of_boolean_input_def by auto
+
+lemma vacio_es_mayor: "bool_vec_from_simplice \<sigma> \<le> bool_vec_from_simplice {}"
+  by (simp add: bool_vec_from_simplice_def less_eq_vec_def)
+
+lemma vacio_siempre_true: "(f::(bool^'n \<Rightarrow> bool)) (bool_vec_from_simplice \<sigma>) \<and> monotone_bool_fun f \<Longrightarrow> f (bool_vec_from_simplice {})"
+  by (meson UNIV_I le_boolE mono_onD monotone_bool_fun_def vacio_es_mayor)
 
 lemma eq_union_or: 
 "simplicial_complex_induced_by_monotone_boolean_function (bool_fun_or f g)
@@ -184,45 +193,18 @@ next
 qed
 
 
-lemma eq_times: 
-"simplicial_complex_induced_by_monotone_boolean_function_times (bool_fun_times f g)
-= (simplicial_complex_induced_by_monotone_boolean_function f) \<times> (simplicial_complex_induced_by_monotone_boolean_function g)"
+lemma eq_times_parcial: 
+  assumes fmon: "monotone_bool_fun f" and gmon: "monotone_bool_fun g"
+  shows "\<gamma> \<in> simplicial_complex_induced_by_monotone_boolean_function_times (bool_fun_times f g)
+\<Longrightarrow> (set_fst \<gamma>, set_snd \<gamma>) \<in> (simplicial_complex_induced_by_monotone_boolean_function f) \<times> (simplicial_complex_induced_by_monotone_boolean_function g)"
 proof
-  show "(simplicial_complex_induced_by_monotone_boolean_function f) \<times> (simplicial_complex_induced_by_monotone_boolean_function g) \<subseteq> simplicial_complex_induced_by_monotone_boolean_function_times (bool_fun_times f g)"
-  proof
-    fix \<gamma>::"('a set)*('b set)"
-    assume pert: "\<gamma> \<in> (simplicial_complex_induced_by_monotone_boolean_function f) \<times> (simplicial_complex_induced_by_monotone_boolean_function g)"
-    hence "(fst \<gamma>) \<in> simplicial_complex_induced_by_monotone_boolean_function f"
-      by auto
-    hence sigma: "f (bool_vec_from_simplice (fst \<gamma>))"
-      by (metis (mono_tags, lifting) bool_vec_set_from_simplice_set_def boolean_function_from_simplicial_complex_def boolean_function_from_simplicial_complex_simplicial_complex_induced_by_monotone_boolean_function_id mem_Collect_eq)
-    from pert have "(snd \<gamma>) \<in> simplicial_complex_induced_by_monotone_boolean_function g"
-      by auto
-    hence tau: "g (bool_vec_from_simplice (snd \<gamma>))"
-      by (metis (mono_tags, lifting) bool_vec_set_from_simplice_set_def boolean_function_from_simplicial_complex_def boolean_function_from_simplicial_complex_simplicial_complex_induced_by_monotone_boolean_function_id mem_Collect_eq)
-    from sigma and tau have sigtau: "bool_fun_times f g (bool_vec_from_simplice ((fst \<gamma>) \<times> (snd \<gamma>)))"
-      using simp_ceros
-      unfolding bool_fun_times_def
-      unfolding set_fst_def
-      unfolding set_snd_def
-      by simp
-    from sigma and tau show "\<gamma> \<in> simplicial_complex_induced_by_monotone_boolean_function_times (bool_fun_times f g)"
-      using bool_vec_from_simplice_def ceros_of_boolean_input_in_set mem_Collect_eq simplicial_complex_induced_by_monotone_boolean_function_times_def
-      by blast
-  qed
-next
-  show "simplicial_complex_induced_by_monotone_boolean_function_ast (bool_fun_ast f g) \<subseteq> set_ast (simplicial_complex_induced_by_monotone_boolean_function f) (simplicial_complex_induced_by_monotone_boolean_function g)"
-  proof
-    fix \<gamma>::"'a set*'b set"
-    assume pert: "\<gamma> \<in> simplicial_complex_induced_by_monotone_boolean_function_ast (bool_fun_ast f g)"
-    hence sigma: "(fst \<gamma>) \<in> simplicial_complex_induced_by_monotone_boolean_function f"
-      by (smt (verit) bool_fun_ast_def fst_es_simplice mem_Collect_eq simplicial_complex_induced_by_monotone_boolean_function_def)
-    from pert have tau: "(snd \<gamma>) \<in> simplicial_complex_induced_by_monotone_boolean_function g"
-      by (smt (verit) bool_fun_ast_def snd_es_simplice mem_Collect_eq simplicial_complex_induced_by_monotone_boolean_function_def)
-    from sigma and tau show "\<gamma> \<in> set_ast (simplicial_complex_induced_by_monotone_boolean_function f) (simplicial_complex_induced_by_monotone_boolean_function g)"
+    fix \<gamma>::"('a*'b) set"
+    assume pert: "\<gamma> \<in> simplicial_complex_induced_by_monotone_boolean_function_times (bool_fun_times f g)"
+    hence funtrue: "bool_fun_times f g (bool_vec_from_simplice ((set_fst \<gamma>) \<times> (set_snd \<gamma>)))"
+
+    thus "(set_fst \<gamma>, set_snd \<gamma>) \<in> (simplicial_complex_induced_by_monotone_boolean_function f) \<times> (simplicial_complex_induced_by_monotone_boolean_function g)"
       using simp_ceros
       by force
-  qed
 qed
 
 end
